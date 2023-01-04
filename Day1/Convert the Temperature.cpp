30f8f9d323a27e6077331956ee43510e5054aa31
Anurag Deshmukh <80694110+AVDeshmukh@users.noreply.github.com>
2023-01-05 02:42:06 +0530

    Update Convert the Temperature.cpp

--- Day1/Convert the Temperature.cpp
+++ Day1/Convert the Temperature.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     vector<double> convertTemperature(double celsius) {
-        vector<double>c;
+        vector<double>c; 
         double f=celsius * 1.80 + 32.00;
         double k=celsius + 273.15;
         c.push_back(k);
