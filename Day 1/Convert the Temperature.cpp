b769eff8524e12b20af5e40b34483b8296e68325
Anurag Deshmukh <80694110+AVDeshmukh@users.noreply.github.com>
2023-01-06 20:32:43 +0530

    Add files via upload

---
+++ Day 1/Convert the Temperature.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    vector<double> convertTemperature(double celsius) {
+        vector<double>c;
+        double f=celsius * 1.80 + 32.00;
+        double k=celsius + 273.15;
+        c.push_back(k);
+        c.push_back(f);
+        return c;
+    }
+};
