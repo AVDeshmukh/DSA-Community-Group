3b4ccf1a2fd790774d3f792b8974ca5adfb7a1a8
Abhishek Raut <102864290+abhishekraut1@users.noreply.github.com>
2023-02-07 21:24:57 +0530

    Create Spirally traversing a matrix.cpp

---
+++ Day 36/Spirally traversing a matrix.cpp
@@ -0,0 +1,36 @@
+class Solution
+{   
+    public: 
+    //Function to return a list of integers denoting spiral traversal of matrix.
+    vector<int> spirallyTraverse(vector<vector<int> > matrix, int r, int c) 
+    {
+        int left = 0;
+        int right = c-1;
+        int top = 0;
+        int bottom = r-1;
+        vector<int> ans;
+        while(left<=right && top<=bottom){
+            for(int i = left; i<=right ; i++){
+                ans.push_back(matrix[top][i]);
+            }
+            top++;
+            for(int i = top; i<=bottom ; i++){
+                ans.push_back(matrix[i][right]);
+            }
+            right--;
+            if(top<=bottom){
+                for(int i = right ; i>=left ; i--){
+                    ans.push_back(matrix[bottom][i]);
+                }
+                bottom--;
+            }
+            if(left<=right){
+                for(int i = bottom ; i>=top; i--)                {
+                    ans.push_back(matrix[i][left]);
+                }
+                left++;
+            }
+        }
+        return ans;
+    }
+};
